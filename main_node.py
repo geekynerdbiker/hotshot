1e2a827ba5a576eff7f737302f76ee4a28793893
Jacob An <lightyear.thespaceranger@gmail.com>
2021-01-01 19:54:01 +0900

    Update main_node.py

--- main_node.py
+++ main_node.py
@@ -16,12 +16,12 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 
-BLUE_COLOR = (255,0,0)
-RED_COLOR = (0,0,255)
-ORANGE_COLOR = (255,0,255)
-GREEN_COLOR = (0,255,0)
-YELLOW_COLOR = (0,255,255)
-WHITE_COLOR = (255,255,255)
+BLUE_COLOR = (255, 0, 0)
+RED_COLOR = (0, 0, 255)
+ORANGE_COLOR = (255, 0, 255)
+GREEN_COLOR = (0, 255, 0)
+YELLOW_COLOR = (0, 255, 255)
+WHITE_COLOR = (255, 255, 255)
 
 odomLength = 200
 goalLength = 150
@@ -36,9 +36,11 @@ angularVelocity = 0
 
 bridge = CvBridge()
 
+
 def threaded_function(arg):
     global isSongEnd
     isSongEnd = True
+
 
 # ------------------CALLBACK FUNCTION----------------------
 def LaserCallback(scan_msg):
@@ -50,45 +52,51 @@ def LaserCallback(scan_msg):
     global isSongEnd
 
     # ----- Image Setting -----
-    laserImage = np.zeros((500,500,3), np.uint8)
+    laserImage = np.zeros((500, 500, 3), np.uint8)
     c_x = 250
     c_y = 250
 
-    angle=0
+    angle = 0
     avoidRadArray = []
     for scanData in scan_msg.ranges:
         COLOR = WHITE_COLOR
         scanRad = angle * math.pi / 360
         scanData *= scanLength
 
-        if scanRad > 140.0*math.pi/180.0 and scanRad <220.0*math.pi/180.0: # front standard -pi/2 ~ pi/2
+        if scanRad > 140.0 * math.pi / 180.0 and scanRad < 220.0 * math.pi / 180.0:  # front standard -pi/2 ~ pi/2
             COLOR = BLUE_COLOR
-            if scanData < 100 and scanData !=0:
+            if scanData < 100 and scanData != 0:
                 COLOR = RED_COLOR
                 avoidRadArray.append(scanRad)
 
-        d_x = scanData*math.cos(scanRad)
-        d_y = scanData*math.sin(scanRad)
-        
+        d_x = scanData * math.cos(scanRad)
+        d_y = scanData * math.sin(scanRad)
+
+        if d_x == float('inf'):
+            d_x = scan_msg.range_max
+        if d_y == float('inf'):
+            d_y = scan_msg.range_max
+
         if scanRad == 0:
-            laserImage = cv2.line(laserImage, (c_x,c_y),(c_x+100,c_y),YELLOW_COLOR,2)
+            laserImage = cv2.line(laserImage, (c_x, c_y), (c_x + 100, c_y), YELLOW_COLOR, 2)
         else:
-            laserImage = cv2.line(laserImage, (c_x,c_y),(c_x+int(d_x),c_y+int(d_y)),COLOR,1)
+            laserImage = cv2.line(laserImage, (c_x, c_y), (c_x + int(d_x), c_y + int(d_y)), COLOR, 1)
         angle += 2
 
-    if len(avoidRadArray)>15: # Failsafe Emergency Stop
-        rospy.loginfo("!!!!!Stop: "+str(len(avoidRadArray))+"!!!!!")
+    if len(avoidRadArray) > 15:  # Failsafe Emergency Stop
+        rospy.loginfo("!!!!!Stop: " + str(len(avoidRadArray)) + "!!!!!")
         isMove = False
-        
+
         if isSongEnd:
             isSongEnd = False
-            thread1 = Thread(target = threaded_function, args = (10, ))
+            thread1 = Thread(target=threaded_function, args=(10,))
             thread1.start()
 
     else:
-        rospy.loginfo("Len: "+str(len(avoidRadArray)))
+        rospy.loginfo("Len: " + str(len(avoidRadArray)))
         isMove = True
-        
+
+
 def OdomCallback(odom_msg):
     global odomRad
     # rospy.loginfo("OdomCallback")
@@ -97,10 +105,11 @@ def OdomCallback(odom_msg):
     o_z = odom_msg.pose.pose.orientation.z
     o_w = odom_msg.pose.pose.orientation.w
 
-    odomRad = -math.atan2(2*(o_y*o_x+o_w*o_z),o_w*o_w+o_x*o_x-o_y*o_y-o_z*o_z)
+    odomRad = -math.atan2(2 * (o_y * o_x + o_w * o_z), o_w * o_w + o_x * o_x - o_y * o_y - o_z * o_z)
     # rospy.loginfo(math.degrees(odomRad))
-    
-def RawCallback(image_msg): # Main Call Back
+
+
+def RawCallback(image_msg):  # Main Call Back
     global rawImage
     rawImage = bridge.imgmsg_to_cv2(image_msg, "bgr8")
 
@@ -108,10 +117,12 @@ def RawCallback(image_msg): # Main Call Back
     RobotMovePublish()
     ImageShow()
 
+
 prevTime = 0
 
+totalFinalX = 0
 
-totalFinalX = 0 
+
 def LineTracing():
     global rawImage
     global angularVelocity
@@ -120,65 +131,63 @@ def LineTracing():
     global error
     global totalFinalX
 
-    now = rospy.get_rostime()    
+    now = rospy.get_rostime()
     nowTime = now.nsecs
     errorTime = 0
-    if nowTime<prevTime:
-        errorTime = nowTime-prevTime+1000000000
+    if nowTime < prevTime:
+        errorTime = nowTime - prevTime + 1000000000
     else:
-        errorTime = nowTime-prevTime
-    errorTime = errorTime / 100000000.0 
+        errorTime = nowTime - prevTime
+    errorTime = errorTime / 100000000.0
     prevTime = nowTime
 
     img_hsv = cv2.cvtColor(rawImage, cv2.COLOR_BGR2HSV)
 
     lower_color = (12, 87, 0)
     upper_color = (44, 255, 255)
-    img_mask = cv2.inRange(img_hsv, lower_color, upper_color) 
-   
-    kernel = np.ones((3, 3), np.uint8)
-    img_mask = cv2.erode(img_mask, kernel, iterations = 2)
-    img_mask = cv2.dilate(img_mask, kernel, iterations = 2)
+    img_mask = cv2.inRange(img_hsv, lower_color, upper_color)
 
-    edgesImage = cv2.Canny(img_mask,50,150,apertureSize = 3)
-    lines = cv2.HoughLinesP(edgesImage, 0.8, np.pi / 180, 90, minLineLength = 50, maxLineGap = 100)
+    kernel = np.ones((3, 3), np.uint8)
+    img_mask = cv2.erode(img_mask, kernel, iterations=2)
+    img_mask = cv2.dilate(img_mask, kernel, iterations=2)
+
+    edgesImage = cv2.Canny(img_mask, 50, 150, apertureSize=3)
+    lines = cv2.HoughLinesP(edgesImage, 0.8, np.pi / 180, 90, minLineLength=50, maxLineGap=100)
 
     if lines is None:
         return None
-
 
     x_array = []
     y_array = []
 
     for i in lines:
 
-        x1=i[0][0]
-        y1=i[0][1]
-        x2=i[0][2]
-        y2=i[0][3]
+        x1 = i[0][0]
+        y1 = i[0][1]
+        x2 = i[0][2]
+        y2 = i[0][3]
 
         if y1 != y2:
-            length = math.sqrt(math.pow(int(x2 - x1), 2) + math.pow(int(y2 - y1),2))
+            length = math.sqrt(math.pow(int(x2 - x1), 2) + math.pow(int(y2 - y1), 2))
             if length > 100:
                 x_array.append(i[0][0])
                 y_array.append(i[0][1])
                 x_array.append(i[0][2])
                 y_array.append(i[0][3])
                 cv2.line(rawImage, (i[0][0], i[0][1]), (i[0][2], i[0][3]), (0, 0, 255), 2)
-                
 
-    if(len(x_array)>3):
-        f1 = np.polyfit(y_array,x_array,1)
-        
+    if (len(x_array) > 3):
+        f1 = np.polyfit(y_array, x_array, 1)
+
         final_y = 380
-        final_x = int(f1[0]*380+f1[1])
-        cv2.circle(rawImage, (final_x,final_y), 5 ,(255, 255, 0), 8)
+        final_x = int(f1[0] * 380 + f1[1])
+        cv2.circle(rawImage, (final_x, final_y), 5, (255, 255, 0), 8)
 
-        error = (320 - final_x)/1000.0
+        error = (320 - final_x) / 1000.0
 
         K_P = 1.1
-        
-        goal = error*K_P
+
+        goal = error * K_P
 
         max_value = 0.45
 
@@ -190,7 +199,6 @@ def LineTracing():
         # print(goal)
 
         angularVelocity = goal
-        
 
 
 # ------------------PUBLISH FUNCTION-----------------------
@@ -212,21 +220,24 @@ def RobotMovePublish():
     if not isMove:
         twist.linear.x = 0
         twist.angular.z = 0
-    
+
     twistPub.publish(twist)
-    
+
+
 def ImageShow():
     global laserImage
     global rawImage
     global edgesImage
 
     cv2.imshow('rawImage', rawImage)
-    cv2.imshow('edgesImage', edgesImage)   
+    cv2.imshow('edgesImage', edgesImage)
     cv2.waitKey(1)
     cv2.imshow('laserImage', laserImage)
 
+
 def initialize():
     print("Initialize")
+
 
 # ---------------------------------------------------------------
 
@@ -242,5 +253,3 @@ laserSub = rospy.Subscriber('/scan', LaserScan, LaserCallback, queue_size=1)
 odomSub = rospy.Subscriber('/odom', Odometry, OdomCallback, queue_size=1)
 
 rospy.spin()
-
-
